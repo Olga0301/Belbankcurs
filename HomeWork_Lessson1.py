edb4a8e6c9c896398f1a21143f8a5f3dc6c26dda
Olga0301 <olga0301888@gmail.com>
2023-02-18 21:05:07 +0300

    initial commit

---
+++ HomeWork_Lessson1.py
@@ -0,0 +1,12 @@
+#text = 'My name is Olga'
+#text1 = text.replace(' ', '-')
+#print(text1)
+#words = text.split( )
+#text2 = '-'.join(words)
+#print(text2)
+
+a = 1
+b = 1
+c = 3
+d = (a + b + c)/3
+print(round(d,3))
--- Lessson1.py
+++ Lessson1.py
@@ -1,3 +1,7 @@
-print('hello')
-print('hello')
+text = 'My name is Olga'
+text1 = text.replace(' ', '-')
+print(text1)
+words = text.split( )
+text2 = '-'.join(words)
+print(text2)
 
